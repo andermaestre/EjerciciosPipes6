f01731fdf6d5d4e98a2fb0e54acaa5da00135d0a
Ander Maestre <andermaestre_90@hotmail.es>
2019-10-23 12:58:46 +0200

    Inicio

---
+++ EjercicioPipes6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace EjerciciosPipes6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lim = 5;
+            for (int i = 0; i < 2; i++)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                using (AnonymousPipeServerStream apcs = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable))
+                {
+                    using (StreamWriter sw = new StreamWriter(apcs))
+                    {
+                        Process p = new Process();
+                        psi.FileName = "..\\..\\..\\Hijo\\bin\\Debug\\Hijo.exe";
+                        psi.UseShellExecute = false;
+                        psi.Arguments = apcs.GetClientHandleAsString();
+                        p.StartInfo = psi;
+
+                        p.Start();
+                        sw.WriteLine(i);
+                        sw.WriteLine(lim);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hijo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace Hijo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont;
+            int lim;
+            using (AnonymousPipeClientStream pipe = new AnonymousPipeClientStream(PipeDirection.In, args[0]))
+            {
+
+                using (StreamReader sr = new StreamReader(pipe))
+                {
+                    cont = int.Parse(sr.ReadLine());
+                    lim = int.Parse(sr.ReadLine());
+                }
+            }
+
+            if (cont == 0)
+            {
+                using (NamedPipeClientStream pipa = new NamedPipeClientStream(".", "pepepipe", PipeDirection.InOut))
+                {
+
+                    using (StreamWriter sw = new StreamWriter(pipa))
+                    {
+                        using (StreamReader sr = new StreamReader(pipa))
+                        {
+                            pipa.Connect();
+
+                            sw.AutoFlush = true;
+
+                            do
+                            {
+                                lim--;
+
+                                sw.WriteLine("ping");
+
+                            } while (lim != 0);
+
+                        }
+
+                    }
+
+                }
+            }
+            else
+            {
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
